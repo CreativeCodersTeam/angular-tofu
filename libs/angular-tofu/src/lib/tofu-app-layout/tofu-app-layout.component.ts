faffe7d4db0178bddcbdecf2de77c57f14163a79
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-04-07 18:31:20 +0200

    Add TofuAppLayout component and module
    Introduced the TofuAppLayout component with its HTML, SCSS, and test files. Created the TofuAppLayout module and configured it to include the component, laying the groundwork for future app layout features.

---
+++ libs/angular-tofu/src/lib/tofu-app-layout/tofu-app-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-tofu-app-layout',
+  standalone: false,
+  templateUrl: './tofu-app-layout.component.html',
+  styleUrl: './tofu-app-layout.component.scss'
+})
+export class TofuAppLayoutComponent {
+
+}
---
+++ libs/angular-tofu/src/lib/tofu-app-layout/tofu-app-layout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TofuAppLayoutComponent } from './tofu-app-layout.component';
+
+
+
+@NgModule({
+  declarations: [TofuAppLayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TofuAppLayoutModule { }
