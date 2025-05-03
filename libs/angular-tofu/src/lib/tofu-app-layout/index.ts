ebfdd1c57b873b4a62aeee9455c4963aecd7b48d
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-05-03 16:02:08 +0200

    Refactor CI workflows and remove unused navigation service
    Reorganized GitHub Actions workflows into distinct files for better clarity and added npm publishing on the main branch. Removed the deprecated `TofuNavigationService` and related test files to clean up unused code. Updated module exports to reflect these changes.

--- libs/angular-tofu/src/lib/tofu-app-layout/index.ts
+++ libs/angular-tofu/src/lib/tofu-app-layout/index.ts
@@ -2,8 +2,6 @@ export * from './tofu-app-layout.component';
 
 export * from '../tofu-navigation-tree/tofu-navigation-entry';
 
-export * from './tofu-navigation.service';
-
 export * from './tofu-app-layout-controller.service';
 
 export * from './tofu-app-layout-consts';
