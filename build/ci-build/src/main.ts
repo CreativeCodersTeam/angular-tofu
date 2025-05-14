6a37ddc8de4683a8c7da398656eddee1614e1f96
darthsharp <48331467+darthsharp@users.noreply.github.com>
2025-05-14 22:29:00 +0200

    Add logging to indicate version calculation process
    Added a log message to clarify when the git version calculation starts and displays the calculated version. This improves visibility into the versioning workflow during the build process.

--- build/ci-build/src/main.ts
+++ build/ci-build/src/main.ts
@@ -1,7 +1,8 @@
 import { SimpleGitVersion } from './simple-git-version';
 
 async function main() {
-  //const { SimpleGitVersion } = await import('./simple-git-version');
+  console.log('Calculating version from git...');
+
   const simpleGitVersion = new SimpleGitVersion();
   const version = await simpleGitVersion.getVersion('alpha');
   console.log('Version:', version);
